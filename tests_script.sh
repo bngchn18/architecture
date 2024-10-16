bba88f84a46ea7154ab979a97f2ba89564b6ef27
bngchn18 <146021085+bngchn18@users.noreply.github.com>
2024-10-16 20:13:00 +0300

    Update tests_script.sh

--- tests_script.sh
+++ tests_script.sh
@@ -27,16 +27,5 @@ run_test "Error: The percent is not in input" "/tmp" "" "5"
 # Тест 4: Неправильный count
 run_test "Error: The count is not in input" "/tmp" "50" ""
 
-# Тест 5: Правильные параметры (папка для тестирования)
-mkdir -p /tmp/test_folder
-echo "test file" > /tmp/test_folder/test1.txt
-echo "another test file" > /tmp/test_folder/test2.txt
-
-# Тест 6: Достаточно свободного места (папка существует)
+# Тест 5: Достаточно свободного места (папка существует)
 run_test "No archiving needed." "/tmp/test_folder" "10" "1"
-
-# Удаление тестовых файлов
-rm -rf /tmp/test_folder
-
-# Удаление временных файлов
-rm -rf /tmp/archive_limit.*
